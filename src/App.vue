fa823ca7cb2bc6914bd923337eebd4fe8d2ef3de
bachtung144 <bachtung144007@gmail.com>
2023-05-06 22:29:19 +0900

    FORM in vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div>
+      <h2>Event Form</h2>
+<!--      <EventTitle-->
+<!--          :value="eventData.title"-->
+<!--          @input="eventData.title = $event"-->
+<!--      />-->
+      <EventTitle v-model="eventData.title"/>
+    </div>
+
+    <div>
+      <h2>Event Form</h2>
+      <label for="title">People</label>
+      <input id="title" type="number" v-model.number="eventData.maxNumber">
+      <p>{{eventData.maxNumber}}</p>
+    </div>
+
+    <div>
+      <h2>Event Form Text</h2>
+      <label for="title">People</label>
+      <input id="title" type="text" v-model.trim="eventData.detail">
+
+      <br>
+
+      <textarea id="detail" v-model="eventData.detail"></textarea>
+      <pre>{{eventData.detail}}</pre>
+
+      <br>
+
+      <input type="checkbox" id="isPrivate" v-model="eventData.isPrivate">
+      <label for="isPrivate"> private </label>
+      <p>{{eventData.isPrivate}}</p>
+
+      <br>
+
+      <p>many checkbox</p>
+      <input type="checkbox" id="10" value="10s" v-model="eventData.target">
+      <label for="10">10s</label>
+      <input type="checkbox" id="20" value="20s" v-model="eventData.target">
+      <label for="20">20s</label>
+      <input type="checkbox" id="30" value="30s" v-model="eventData.target">
+      <label for="30">30s</label>
+      <p>{{eventData.target}}</p>
+
+      <br>
+      <p>Radio checkbox</p>
+      <input type="radio" id="free" value="free" v-model="eventData.price">
+      <label for="free">free</label>
+      <input type="radio" id="paid" value="paid" v-model="eventData.price">
+      <label for="paid">paid</label>
+
+      <select v-model="eventData.location" multiple>
+        <option v-for="location in locations" :key="location">{{location}}</option>
+      </select>
+      <p>{{eventData.location}}</p>
+
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import EventTitle from "@/components/EventTitle.vue";
 
 export default {
   name: 'App',
+  data() {
+    return {
+      eventData: {
+        title: "title",
+        maxNumber: 20,
+        detail: "",
+        isPrivate: false,
+        target: [],
+        price: "free",
+        location: []
+      },
+      locations: ['tokyo', 'nagoya', 'osaka']
+    }
+  },
   components: {
-    HelloWorld
+    EventTitle
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/EventTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <label for="title">title</label>
+    <input id="title"
+           type="text"
+           :value="value"
+           @input="$emit('input', $event.target.value)"
+    >
+    <p>{{value}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventTitle",
+  props: ['value']
+}
+</script>
